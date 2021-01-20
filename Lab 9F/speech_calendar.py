b65684137519713ec502b97bacb0c68f26967c3f
fhn <fhn037@126.com>
2021-01-20 14:59:27 +0800

    :fire:delete a test code

--- Lab 9F/speech_calendar.py
+++
@@ -1,6 +0,0 @@
-from aip import AipSpeech
-APP_ID = '23474300'
-API_KEY = 'sluLEi1xoOTdgC7hU570Pebl'
-SECRET_KEY = 'YmQxWR9Cn5rex5LQbrwl8DGr3BbkMYTg'
-
-client = AipSpeech(APP_ID, API_KEY, SECRET_KEY)
